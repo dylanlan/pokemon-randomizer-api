0a4e46a61efd0a20e5535db4af6c9962bb4b25ff
Dylan Stankievech <dylan.stank@gmail.com>
2019-05-14 22:05:27 -0600

    Initial attempts to display pokemon

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,13 @@ router.get('/', function (req, res) {
     res.status(200).end();
 });
 
+router.get('/randomize', function (req, res) {
+    res.render('index', {
+        pokemon: [
+            'Pikachu'
+        ]
+    });
+    res.status(200).end();
+});
+
 module.exports = router;
