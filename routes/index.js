7911a5eaf32f862adce131c6ee8aa614700091e3
Dylan Stankievech <dylan.stank@gmail.com>
2019-05-16 21:38:11 -0600

    Added an api endpoint

--- routes/index.js
+++ routes/index.js
@@ -10,13 +10,26 @@ router.get('/', function (req, res) {
 });
 
 router.get('/randomize', async function (req, res) {
-    const randomPokemon = await randomizer.pickRandomPokemon({
-        number: 6
-    });
+    const randomPokemon = await getRandomPokemon(getDefaultOptions());
     res.render('index', {
         pokemon: randomPokemon.map(p => p.name)
     });
     res.status(200).end();
 });
+
+router.get('/api/randomize', async function (req, res) {
+    const randomPokemon = await getRandomPokemon(getDefaultOptions());
+    res.send(JSON.stringify(randomPokemon, null, 2));
+});
+
+const getRandomPokemon = async (options) => {
+    return await randomizer.pickRandomPokemon(options);
+};
+
+const getDefaultOptions = () => {
+    return {
+        number: 6
+    };
+};
 
 module.exports = router;
